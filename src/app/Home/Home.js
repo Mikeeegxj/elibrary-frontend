3b7909bdc464c92d01fffdd7ed88110e8ca6cbb3
Mikeeegxj <xiaojun.guo@my.jcu.edu.au>
2024-06-19 11:58:56 +0800

    added top book session

--- src/app/Home/Home.js
+++ src/app/Home/Home.js
@@ -1,10 +1,34 @@
 "use client"
 import { Box, Stack, Text, Container, HStack, Image, VStack, Button } from "@chakra-ui/react"
 import { montserrat, nunito_sans } from "../fonts"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './Book-swiper-styles.css';
+import { Pagination } from 'swiper/modules';
+
+import BookCard from "../components/book-card/BookCard";
 export default function Home() {
+    const books = [{
+        name: "Really Good Actually",
+        url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSychbQIpvpgPKGVx0r75hjROWLFWQrrmPYu3u37aEk5P0qrd9z3Kg5C9Ssd9WFPcPyVWTO1Uvd8ATcgnPRqAlVlg_HveBWI7fmnLn7xmY&usqp=CAE"
+    },
+    {
+        name: "Educated",
+        url: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1506026635i/35133922.jpg"
+    },
+    {
+        name: "Deathly Hallow",
+        url: "https://images.booksense.com/images/221/010/9780545010221.jpg"
+    },
+    {
+        name:"The Outsider",
+        url: "https://m.media-amazon.com/images/I/71Bg39CmhoL._AC_UF350,350_QL50_.jpg"
+    }
+    ]
     return (
         <Container maxW={"96em"} my={8}>
-            <HStack flexDir={{base:"column", md:"row"}}>
+            <HStack mb={12} flexDir={{ base: "column", md: "row" }}>
                 <VStack alignItems={"flex-start"} spacing={8} flex={1}>
                     <Text color={""} fontWeight={500} fontSize={{ base: "30px", md: "38px" }} className={`${montserrat.className}`}>
                         Welcome to E-Library Hub
@@ -16,10 +40,49 @@ export default function Home() {
                         Sign Up
                     </Button>
                 </VStack>
-                <VStack display={{base:"block", md:"block"}} width={"100%"} flex={1}>
+                <VStack display={{ base: "block", md: "block" }} width={"100%"} flex={1}>
                     <Image src={"/assets/coverbook.png"} />
                 </VStack>
             </HStack>
+            <Box>
+                <Text className={`${montserrat.className}`} fontWeight={400} fontSize={{ base: "20px", md: "24px" }}>
+                    Top Books
+                </Text>
+                <Box width={"100%"} height={"100%"}>
+                    <Swiper
+                        slidesPerView={1}
+                        spaceBetween={10}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        breakpoints={{
+                            768: {
+                                slidesPerView: 2,
+                                spaceBetween: 10,
+                            },
+                            1000: {
+                                slidesPerView: 3,
+                                spaceBetween: 20,
+                            },
+                            1800: {
+                                slidesPerView: 4,
+                                spaceBetween: 20,
+                            },
+                        }}
+                        modules={[Pagination]}
+                        className="mySwiper book-swiper"
+                        style={{
+                            paddingBottom: "30px",
+                        }}
+                    >
+                        {
+                            books.map((book, index) => (
+                                <SwiperSlide key={index}> <BookCard book={book}/> </SwiperSlide>
+                            ))
+                        }
+                    </Swiper>
+                </Box>
+            </Box>
         </Container>
     )
 }
---
+++ src/app/components/book-card/BookCard.js
@@ -0,0 +1,32 @@
+"use client";
+import { montserrat, nunito_sans, poppins, roboto, work_sans } from "@/app/fonts";
+import { Box, Text, Badge, VStack, Image, HStack, Button } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { IconHeart } from "@tabler/icons-react";
+export default function BookCard({book}) {
+    const router = useRouter()
+    return (
+        <Box transition={"all 0.2s ease-in"} _hover={{ cursor: "pointer", transition: "all 0.2s ease-in", transform: "scale(1.02)" }} borderRadius={"10px"} width={"100%"} boxShadow={"0px 0px 6px -1px rgba(196,196,196,0.75)"}>
+            <HStack alignItems={"flex-end"} spacing={1} p={5}>
+                <Image borderRadius={"12px"} src={book.url}
+                    height={"280px"} width={"100%"} objectFit={"contain"} objectPosition={"center"} />
+                <VStack alignItems={"flex-start"} height={"100%"} width={"100%"} px={3} spacing={4}>
+                    {/* <Badge mb={2} bg={"#3394d7"} fontSize={"10px"} padding={1} variant="solid" borderRadius={"5px"} colorScheme='green'> Nice</Badge> */}
+                    <Text color={"#0D0842"} fontSize={"16px"} className={`${montserrat.className}`} fontWeight={400} noOfLines={1} overflow={"hidden"} sx={{ overflowWrap: 'break-word' }}>
+                        {book.name}
+                    </Text>
+                    <Text mt={1} color={"#0D0842"} fontSize={"14px"} className={`${nunito_sans.className}`} fontWeight={400} noOfLines={3} overflow={"hidden"} sx={{ overflowWrap: 'break-word' }}>
+                        Lindbergh's Pharmacy is an Athens, Georgia, institution...
+                    </Text>
+                    <Text mt={1} color={"#0D0842"} fontSize={"16px"} className={`${nunito_sans.className}`} fontWeight={400} noOfLines={3} overflow={"hidden"} sx={{ overflowWrap: 'break-word' }}>
+                        Free
+                    </Text>
+                    <Button color={"white"} bg={"#FFCE1A"}>
+                        Read
+                    </Button>
+                </VStack>
+            </HStack>
+
+        </Box>
+    )
+}
